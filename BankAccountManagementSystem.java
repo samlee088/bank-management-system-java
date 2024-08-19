33b41c6bfd4752c6ccda4a8ba160e4cd32a9aaa1
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-19 16:46:53 -0700

    initialize bank account management system, create account ,deposti

---
+++ BankAccountManagementSystem.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class BankAccountManagementSystem {
+    
+    private Map<String, Double> accounts;
+
+    public BankAccountManagementSystem() {
+        accounts = new HashMap<>();
+    }
+
+    public void createAccount(String accountNumber) {
+        if (accounts.containsKey(accountNumber)) {
+            System.out.println("Account already exists");
+        } else {
+            accounts.put(accountNumber, 0.0);
+            System.out.println("Account created successfully");
+        }
+    }
+
+    public void deposit(String accountNumber, double amount) {
+        if(accounts.containsKey(accountNumber)) {
+            double balance = accounts.get(accountNumber);
+            balance += amount;
+            accounts.put(accountNumber, balance);
+            System.out.println("Deposit Successfully");
+        } else {
+            System.out.println("Account does not exist");
+        }
+    }
+}
